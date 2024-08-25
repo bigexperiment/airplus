0c4fe10d7119cc1e72a8c272d4c12c6add7816c6
bigexperiment <ganesha.727@gmail.com>
2024-08-24 18:22:26 -0700

    alds

--- src/pages/Everest.js
+++ src/pages/Everest.js
@@ -5,7 +5,7 @@ const Everest = () => {
     <div className="font-['Poppins',sans-serif] bg-slate-50 text-slate-800 h-screen w-screen flex p-4">
       <div className="flex-grow w-full h-full">
         <iframe
-          src="https://docs.google.com/document/d/1fBFmpkdW0h_XsHrWcIrGPYGC0zgwUIgU-f_Ht1GO9hI/pub?embedded=true"
+          src="https://docs.google.com/document/d/1nX0QzYU2wwWjpuQGjduQpcx4zl3tHFKrr_gysb9x0XI/pub"
           className="w-full h-full rounded-lg shadow-lg"
           title="Everest Document"
         />
